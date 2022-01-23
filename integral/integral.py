5094bc3a0fc4e991b4f9ef224b03293cb334c1d7
Planck <maxplanck817@gmail.com>
2022-01-23 15:19:58 +0530

    FIX: trapezoidal method

--- integral/integral.py
+++ integral/integral.py
@@ -1,18 +1,19 @@
 import numpy as np 
 from scipy.integrate import quad
 
-def trapezoidal_rule(a,b,f,n=2**4,n_max=2**16,rtol=0.5e-6):
+def trapezoidal_rule(a,b,f,n=2**4,n_max=2**32,rtol=0.5e-6):
     nstart,nstop = np.log2(n),np.log2(n_max)
     n_array = np.logspace(nstart,nstop,base=2,num = int(nstop-nstart+1))
     I = np.zeros(n_array.shape)
     h = (b-a)/n_array
     x = np.linspace(a,b,int(n_array[0]+1))
+    print(x)
     y = f(x)
     I[0] = (h[0]/2)*np.sum(y[:-1] + y[1:])
     for i in np.arange(1,n_array.shape[0]):
-        x = np.linspace(a,b,int(n_array[i]+1))
+        x = np.linspace(a,b,int(n_array[i-1]+1))
         modedx = (x[:-1] +x[1:])/2
-        I[i] = (1/2)*I[i-1] + h[i]/2*(np.sum(f(modedx)))
+        I[i] = (1/2)*I[i-1] + h[i]*(np.sum(f(modedx)))
         if np.abs((I[i]-I[i-1])/I[i]) <= rtol:
             return I[i],0
     return I[-1],-1

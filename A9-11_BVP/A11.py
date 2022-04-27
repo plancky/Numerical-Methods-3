e2f15c7ec802f7fc334f331922a4a8d74b0fab0c
planck <maxplanck817@gmail.com>
2022-04-28 00:11:52 +0530

    Ordinary BVP FDM solver, major

---
+++ A9-11_BVP/A11.py
@@ -0,0 +1,71 @@
+import numpy as np
+from numpy import vectorize as vec
+def get_tridiag(l,d,u):
+    N = len(d)
+    if not (N-1 == len(l) and N-1 ==len(u)):
+        raise ValueError(f"length of l and d must be {N-1}.")
+    return np.diag(l,-1) + np.diag(d,0) + np.diag(u,1)
+
+#y′′ (x) + p(x)y ′ (x) + q(x)y(x) + r(x) = 0
+class ordinary_bvp:
+    def __init__(self,p,q,r,dom=(0,1)) -> None:
+        self.p = p 
+        self.q = q 
+        self.r = r
+        self.dom = dom
+        pass
+
+    def discretize(self,N:int):
+        self.N = N 
+        self.ddom = np.linspace(self.dom[0],self.dom[1],N+1)
+        self.w = np.zeros(self.ddom.shape) 
+        #ddom,w = [0,1,2....N-1,N]
+        self.h = abs(np.diff(self.ddom)[0])/N
+        h,x = self.h,self.ddom
+        self.d = vec(lambda i : 2 + h**2*self.p(x[i]))
+        self.u = vec(lambda i : -1 + h/2*self.q(x[i]))
+        self.l = vec(lambda i : -1 - h/2*self.q(x[i]))
+        self.b = vec(lambda i : -h**2*self.r(x[i]))  
+        return self.ddom
+
+    def set_dirichlet(self,a,b):
+        self.w[0],self.w[-1] = a,b
+        h,x = self.h,self.ddom
+        self.a11,self.a12 = 1,0
+        self.ann,self.an_1n = 1,0
+        self.b1,self.bn = a,b
+        return self.w
+
+    def set_neumann(self,a,b):
+        self.w[0],self.w[-1] = a,b
+        h,x = self.h,self.ddom
+        self.a11,self.a12 = self.d(0),-2
+        self.ann,self.an_1n = self.d(self.N),-2
+        self.b1,self.bn = self.b(0)+2*h*self.l(0)*a,self.b(self.N)-2*h*self.l(self.N)*b
+        return self.w
+    
+    def set_robin(self,a,b): # a1 y(0) + a2 y'(0) = a3 ; b1 y(N) + b2y'(N) = b3 
+        (a1,a2,a3),(b1,b2,b3) = a,b
+        h,x = self.h,self.ddom
+        r,b,d,l,u,N = self.r,self.b,self.d,self.l,self.u,self.N
+        self.a11,self.a12 = d(0) + 2*h*l(0)*a1/a2,-2
+        self.ann,self.an_1n = d(N) - 2*h*u(N)*b1/b2 ,-2
+        self.b1,self.bn = b(0)+2*h*l(0)*a3/a2, b(N)-2*h*l(N)*b3/b2
+        return self.w
+        
+    def get_A_b(self):
+        ii = np.arange(1,self.N)
+        l_,d_ = np.zeros(self.N),np.zeros(self.N+1)
+        u_,b_ = l_.copy(),d_.copy()
+        l_[:-1],l_[-1] = self.l(ii), self.an_1n 
+        u_[1:],u_[0] = self.u(ii), self.a12 
+        d_[1:-1],d_[0],d_[-1] = self.d(ii),self.a11,self.ann
+        b_[1:-1],b_[0],b_[-1] = self.b(ii),self.b1,self.bn 
+        A = get_tridiag(l_,d_,u_)
+        return A,b_
+
+if __name__ == "__main__":
+    bvp1 = ordinary_bvp(lambda x: np.pi**2,lambda x:0,lambda x:-2*np.pi**2*np.sin(np.pi*x),(0,1))
+    bvp1.discretize(4)
+    bvp1.set_dirichlet(0,0)
+    print(bvp1.get_A_b())
--- A9-11_BVP/A9.py
+++ A9-11_BVP/A9.py
@@ -9,7 +9,7 @@ y1_b = 1
 y_exact = lambda x:3/8*np.sin(x) - np.cos(x) -1/8*np.sin(3*x)
 robin_a = lambda x: -x -1 
 
-def solve_bvp(func,robin,N=100,btype = "nn"):
+def lin_shooting(func,robin,N=100,btype = "nn"):
     def optimize(y0_a):
         p =set_problem(func,[0,np.pi/2],[0,robin(y0_a),y0_a],N)
         o = p.rk4()[-1,:]
@@ -18,10 +18,22 @@ def solve_bvp(func,robin,N=100,btype = "nn"):
     p =set_problem(func,[0,np.pi/2],[0,robin_a(y0),y0],N)
     return(p)
 
-obtained_p = solve_bvp(func,robin_a,N=5)
-obtained_p.rk4()
 f,ax = plt.subplots(1,1)
-obtained_p.jt_plot(ax,1)
 x_space = np.linspace(0,np.pi/2)
 ax.plot(x_space,y_exact(x_space))
+for N in [4,8,16,32,64,128]:
+    obtained_p = lin_shooting(func,robin_a,N)
+    obtained_p.rk4()
+    obtained_p.jt_plot(ax,1)
 plt.show()
+
+for N in [4,8]:
+    tabl = np.zeros((N+1,4))
+    new_p = lin_shooting(func,robin_a,N)
+    rk4_soln = new_p.rk4()
+    tabl[:,1] = rk4_soln[:,1]
+    tabl[:,0] = new_p.dom
+    tabl[:,2] = y_exact(tabl[:,0])
+    tabl[:,3] = np.abs(tabl[:,1]-tabl[:,2])
+    print(tabl)
+    np.savetxt(f"dat_E_N{N}.csv",tabl,fmt="%.8E",delimiter=",")
